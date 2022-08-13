aba9a4783e4e309568a440abe6b6b4d9490e759a
GitHub Action <action@github.com>
2022-08-13 08:35:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/nannany/specialsniffle/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nannany/specialsniffle/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nannany.specialsniffle.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nannany.specialsniffle.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/nannany/specialsniffle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nannany/specialsniffle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nannany.specialsniffle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nannany.specialsniffle.MyBundle
 
 class MyApplicationService {
 
